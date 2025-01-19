1d31979df07f9c5e5956fc8f60bb270aa47fa40b
PolyG0ne <witty.rcj@gmail.com>
2025-01-19 11:33:07 -0500

    Update converter.py

--- fonctions/converter.py
+++ fonctions/converter.py
@@ -1,1 +1,11 @@
 import streamlit as st
+
+option_types = ["Longueurs", "Poids", "Distances", "Liquides", "Température"]
+option_types1 = ["Longueurs", "Poids", "Distances", "Liquides", "Température"]
+
+option_param = []
+convert_choice = []
+
+def call_back():
+    st.write(st.session_state.list_convert)
+    st.write(st.session_state.choice)
