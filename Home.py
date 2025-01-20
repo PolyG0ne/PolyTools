3e92b5681d950d31445ea544b736ed0346a5808e
PolyG0ne <witty.rcj@gmail.com>
2025-01-20 14:59:06 +0000

    .

--- Home.py
+++ Home.py
@@ -4,6 +4,11 @@ import streamlit as st
 if 'lang_' not in st.session_state:
     st.session_state['lang_'] = "FR"
 
+st.set_page_config(
+    page_title="PolyTools",
+    page_icon=":gears:",
+    layout="wide"
+)
 st.title("Bienvenu sur le site PolyTools !")
 
 st.write("Des outils pour tous les besoins au bout des doigts")
