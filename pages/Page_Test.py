c3b5c84792fcc7bebf304f9d06f85d0621726976
PolyG0ne <witty.rcj@gmail.com>
2025-01-20 15:53:37 -0500

    icons

--- pages/Page_Test.py
+++ pages/Page_Test.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.page_link("Home.py", label="Retour", icon=":material/dashboard:")
+st.page_link("Home.py", label="Retour", icon=":material/home:")
 
 st.write("Page pour tester du code")
