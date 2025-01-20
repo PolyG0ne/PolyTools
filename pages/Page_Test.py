fbfacbf4076fdad388edbeefd73bd3774ce9ac5a
PolyG0ne <witty.rcj@gmail.com>
2025-01-20 15:57:13 -0500

    .

--- pages/Page_Test.py
+++ pages/Page_Test.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title(st.page_link("Home.py", label="", icon=":material/home:"))
+st.title(label=':material/home:' icon=":material/home:")
 
 st.write("Page pour tester du code")
