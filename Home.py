2d04efbdd329b8a6ed2595bc60c027b94c4de8d0
PolyG0ne <witty.rcj@gmail.com>
2025-01-21 05:42:23 +0000

    .

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # initialisation Session State
 if 'lang_' not in st.session_state:
-    st.session_state['lang_'] = "FR"
+    st.session_state.lang_ = "FR"
 
 st.set_page_config(
     page_title="PolyTools",
@@ -13,3 +13,4 @@ st.title("Bienvenu sur le site PolyTools !")
 
 st.write("Des outils pour tous les besoins au bout des doigts")
 
+st.session_state
--- pages/Page_Test.py
+++ pages/Page_Test.py
@@ -1,5 +1,19 @@
 import streamlit as st
 
-st.title(st.page_link("Home.py", label="Retour", icon=":material/home:"))
-
+st.title(":material/glyphs:")
 st.write("Page pour tester du code")
+st.divider()
+
+if 'name' not in st.session_state:
+    st.session_state.name = ''
+
+name = st.text_input("Enter your name : ", on_change=None)
+
+def named(name):
+    st.session_state.name = name
+
+name_record = st.button("Save",on_click=named, args=[name])
+st.write(st.session_state.name)
+
+if name_record:
+    st.write("le if du bouton est True")
