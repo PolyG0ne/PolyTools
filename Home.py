b9f772c773fbe3218512afa60c6f0183395fd75b
PolyG0ne <witty.rcj@gmail.com>
2025-01-26 05:17:42 -0500

    .

--- Home.py
+++ Home.py
@@ -14,14 +14,3 @@ st.title("Bienvenu sur le site PolyTools !")
 st.write("Des outils pour tous les besoins au bout des doigts")
 
 st.session_state
-
-st.snow()
-st.toast('Mr Stay-Puft')
-
-# Insert a chat message container.
-with st.chat_message("user"):
-    st.write("Hello 👋")
-    st.line_chart(np.random.randn(30, 3))
-
-# Display a chat input widget.
-st.chat_input("Say something")
